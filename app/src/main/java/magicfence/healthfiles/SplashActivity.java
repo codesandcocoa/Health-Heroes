1e78c7a8a434bc5beca7550ce7e7d13421b11efe
sanjay-thiyagarajan <sanjayipscoc@gmail.com>
2020-10-02 12:39:44 +0530

    Directly takes to Dashboard if the user is already logged in. Irritated coz of signing in everytime

--- app/src/main/java/magicfence/healthfiles/SplashActivity.java
+++ app/src/main/java/magicfence/healthfiles/SplashActivity.java
@@ -1,24 +1,57 @@
 package magicfence.healthfiles;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class SplashActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT=1500;
-
+    FirebaseAuth mAuth;
+    DatabaseReference usersRef;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        mAuth = FirebaseAuth.getInstance();
+        usersRef = FirebaseDatabase.getInstance().getReference().child("Patients");
+
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
-                Intent homeIntent = new Intent(SplashActivity.this,MainActivity.class);
-                startActivity(homeIntent);
-                finish();
+                usersRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.hasChild(mAuth.getCurrentUser().getUid()))
+                        {
+                            Intent homeIntent = new Intent(SplashActivity.this,DashboardActivity.class);
+                            startActivity(homeIntent);
+                            finish();
+                        }
+                        else
+                        {
+                            Intent homeIntent = new Intent(SplashActivity.this,MainActivity.class);
+                            startActivity(homeIntent);
+                            finish();
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
             }
         },SPLASH_TIME_OUT);
     }
