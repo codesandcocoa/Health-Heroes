f584822410b4a7142e52f92d7c66a528652a0e96
sanjay-thiyagarajan <sanjayipscoc@gmail.com>
2020-10-02 10:37:09 +0530

    Added UI to VerifyDoctorActivity

--- app/src/main/java/magicfence/healthfiles/DashboardActivity.java
+++ app/src/main/java/magicfence/healthfiles/DashboardActivity.java
@@ -8,18 +8,27 @@ import android.widget.LinearLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DashboardActivity extends AppCompatActivity {
-    private LinearLayout myQrLT;
+    private LinearLayout myQrLT,verifyLT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         myQrLT = (LinearLayout) findViewById(R.id.llt_myqr);
+        verifyLT = (LinearLayout) findViewById(R.id.verify_dr_llt);
 
         myQrLT.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent qrIntent = new Intent(DashboardActivity.this, MyQRActivity.class);
+                startActivity(qrIntent);
+            }
+        });
+
+        verifyLT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent qrIntent = new Intent(DashboardActivity.this, VerifyDoctorActivity.class);
                 startActivity(qrIntent);
             }
         });
--- app/src/main/java/magicfence/healthfiles/VerifyDoctorActivity.java
+++ app/src/main/java/magicfence/healthfiles/VerifyDoctorActivity.java
@@ -2,13 +2,52 @@ package magicfence.healthfiles;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.zxing.Result;
+
 public class VerifyDoctorActivity extends AppCompatActivity {
+
+    CodeScanner codeScanner;
+    CodeScannerView codeScannerView;
+    FirebaseAuth mAuth;
+    DatabaseReference docRef;
+    String docID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verify_doctor);
+        mAuth = FirebaseAuth.getInstance();
+        docRef = FirebaseDatabase.getInstance().getReference().child("Doctors");
+
+        codeScannerView = (CodeScannerView) findViewById(R.id.qr_scan_view);
+        codeScanner = new CodeScanner(this,codeScannerView);
+
+        codeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        docID = result.getText();
+
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        codeScanner.startPreview();
     }
 }
