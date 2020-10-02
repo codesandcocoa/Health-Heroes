ad923b56847e3c1f22dcff694241012792e1d9ce
sanjay-thiyagarajan <sanjayipscoc@gmail.com>
2020-10-02 10:44:12 +0530

    Implemented functionality for VerifyDoctorActivity

--- app/src/main/java/magicfence/healthfiles/VerifyDoctorActivity.java
+++ app/src/main/java/magicfence/healthfiles/VerifyDoctorActivity.java
@@ -1,6 +1,9 @@
 package magicfence.healthfiles;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,9 +12,18 @@ import com.budiyev.android.codescanner.CodeScanner;
 import com.budiyev.android.codescanner.CodeScannerView;
 import com.budiyev.android.codescanner.DecodeCallback;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class VerifyDoctorActivity extends AppCompatActivity {
 
@@ -38,9 +50,35 @@ public class VerifyDoctorActivity extends AppCompatActivity {
                     @Override
                     public void run() {
                         docID = result.getText();
+                        docRef.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                if (snapshot.hasChild(docID))
+                                {
+                                    Toast.makeText(VerifyDoctorActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                                }
+                                else
+                                {
+                                    Toast.makeText(VerifyDoctorActivity.this, "Not authorized", Toast.LENGTH_SHORT).show();
+                                }
+
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
 
                     }
                 });
+            }
+        });
+
+        codeScannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                codeScanner.startPreview();
             }
         });
     }
@@ -49,5 +87,26 @@ public class VerifyDoctorActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         codeScanner.startPreview();
+        requestForCamera();
+    }
+
+    private void requestForCamera() {
+        Dexter.withActivity(this).withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        codeScanner.startPreview();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                        Toast.makeText(VerifyDoctorActivity.this, "We are not permitted to use your camera", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
     }
 }
