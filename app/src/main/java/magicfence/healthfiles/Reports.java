5fa24cc2fb9a6824f8c53cc6627ec5f50a19a381
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2021-03-02 09:32:18 +0530

    Update Reports.java

--- app/src/main/java/magicfence/healthfiles/Reports.java
+++ app/src/main/java/magicfence/healthfiles/Reports.java
@@ -1,7 +1,12 @@
 package magicfence.healthfiles;
 
+// Model class for holding the reports
+
 public class Reports {
+    
+    // Report title and report date
     String title,date;
+    
     public Reports() {
     }
 
@@ -9,19 +14,23 @@ public class Reports {
         this.title = title;
         this.date = date;
     }
-
+    
+    // Getter for title
     public String getTitle() {
         return title;
     }
-
+    
+    // Setter for title
     public void setTitle(String title) {
         this.title = title;
     }
 
+    // Getter for date
     public String getDate() {
         return date;
     }
 
+    // Setter for date
     public void setDate(String date) {
         this.date = date;
     }
