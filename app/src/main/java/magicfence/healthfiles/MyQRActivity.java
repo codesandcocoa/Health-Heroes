fe4a3028af8baa83da5963e6d9f722ac3dfc5f57
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2021-03-02 09:05:34 +0530

    Update MyQRActivity.java

--- app/src/main/java/magicfence/healthfiles/MyQRActivity.java
+++ app/src/main/java/magicfence/healthfiles/MyQRActivity.java
@@ -1,20 +1,18 @@
 package magicfence.healthfiles;
 
+// IMPORTS
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.widget.ImageView;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.firebase.auth.FirebaseAuth;
-
 import androidmads.library.qrgenearator.QRGContents;
 import androidmads.library.qrgenearator.QRGEncoder;
 
 public class MyQRActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
-    String currentUserID;
+    private String currentUserID;
     private ImageView qrView;
     private Bitmap bitmap;
 
@@ -22,14 +20,18 @@ public class MyQRActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_q_r);
+        
+        // Initialising Firebase services
         mAuth = FirebaseAuth.getInstance();
         currentUserID = mAuth.getCurrentUser().getUid();
 
         qrView = (ImageView) findViewById(R.id.myqr_imview);
 
+        // Generating a QR code for the current user UID
         QRGEncoder qrgEncoder = new QRGEncoder(currentUserID, null, QRGContents.Type.TEXT, 200);
         bitmap = qrgEncoder.getBitmap();
 
+        // Rendering the generated QR Bitmap
         qrView.setImageBitmap(bitmap);
 
     }
