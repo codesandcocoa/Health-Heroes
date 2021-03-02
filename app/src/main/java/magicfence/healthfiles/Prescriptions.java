f2c4c36f43b2bc47135158c505d9ff077c5bb65e
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2021-03-02 09:12:24 +0530

    Update Prescriptions.java

--- app/src/main/java/magicfence/healthfiles/Prescriptions.java
+++ app/src/main/java/magicfence/healthfiles/Prescriptions.java
@@ -1,8 +1,13 @@
 package magicfence.healthfiles;
 
+// Model class for fetching the Prescriptions
+
 public class Prescriptions {
+    
+    // Doctor's name
     String dr_name;
 
+    // Constructors
     public Prescriptions() {
     }
 
@@ -11,10 +16,12 @@ public class Prescriptions {
 
     }
 
+    // GETTER
     public String getDr_name() {
         return dr_name;
     }
 
+    // SETTER
     public void setDr_name(String dr_name) {
         this.dr_name = dr_name;
     }
