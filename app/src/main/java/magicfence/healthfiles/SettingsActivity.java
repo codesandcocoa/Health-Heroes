a5ff218f66223c2f2fd5ed9cebb4196fdd144427
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2021-03-02 09:41:14 +0530

    Update SettingsActivity.java

--- app/src/main/java/magicfence/healthfiles/SettingsActivity.java
+++ app/src/main/java/magicfence/healthfiles/SettingsActivity.java
@@ -1,14 +1,13 @@
 package magicfence.healthfiles;
 
+// IMPORTS
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -28,12 +27,15 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        
+        // Initialising Firebase services
         mAuth = FirebaseAuth.getInstance();
         currentUserID = mAuth.getCurrentUser().getUid();
 
         logoutButton = (Button) findViewById(R.id.logout_button);
         resetButton = (Button) findViewById(R.id.reset_button);
 
+        // Logs out the user and redirects to SplashActivity
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,6 +46,7 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
+        // Resets the password
         resetButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,6 +56,7 @@ public class SettingsActivity extends AppCompatActivity {
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
                         if (snapshot.exists())
                         {
+                            // Getting the email address of the user
                             email = snapshot.child(currentUserID).child("email").getValue().toString();
                         }
 
@@ -63,13 +67,15 @@ public class SettingsActivity extends AppCompatActivity {
 
                     }
                 });
-
+                
+                // Sending password reset email
                 mAuth.sendPasswordResetEmail(email)
                         .addOnCompleteListener(new OnCompleteListener<Void>() {
                             @Override
                             public void onComplete(@NonNull Task<Void> task) {
                                 if (task.isSuccessful())
                                 {
+                                    // Toasts on success
                                     Toast.makeText(SettingsActivity.this, "Password reset link has been sent successfully..!", Toast.LENGTH_SHORT).show();
                                 }
 
