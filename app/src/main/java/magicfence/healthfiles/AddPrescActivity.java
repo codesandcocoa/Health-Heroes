8f3c1a5ab256b5efd498de4247a53f67b8efd8ff
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2021-03-02 08:35:18 +0530

    Update AddPrescActivity.java

--- app/src/main/java/magicfence/healthfiles/AddPrescActivity.java
+++ app/src/main/java/magicfence/healthfiles/AddPrescActivity.java
@@ -37,41 +37,43 @@ import androidmads.library.qrgenearator.QRGEncoder;
 
 public class AddPrescActivity extends AppCompatActivity {
 
-    FirebaseAuth mAuth;
-    String patient_id;
-    String currentUserID;
-    String urll;
-    String dr_name;
-    DatabaseReference patientRef;
-    TextView PatientNameTV,DateTV;
-    Bitmap bitmap;
+    private FirebaseAuth mAuth;
+    private String patient_id;
+    private String currentUserID;
+    private String urll;
+    private String dr_name;
+    private DatabaseReference patientRef;
+    private TextView PatientNameTV,DateTV;
+    private Bitmap bitmap;
     private EditText PrescriptionsET;
     private Button SignPrescButton;
-    ProgressDialog progressDialog;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_presc);
-
+        
+        // Initialising Firebase service
         mAuth = FirebaseAuth.getInstance();
+        
         currentUserID = mAuth.getCurrentUser().getUid();
         patient_id = getIntent().getStringExtra("uid");
         progressDialog = new ProgressDialog(this);
         patientRef = FirebaseDatabase.getInstance().getReference().child("Patients").child(patient_id);
         SignPrescButton = (Button) findViewById(R.id.sign_presc_button);
         PrescriptionsET = (EditText) findViewById(R.id.add_prec_prescription);
-
-
         DateTV = (TextView) findViewById(R.id.add_presc_pdate);
-
         Date c = Calendar.getInstance().getTime();
         System.out.println("Current time => " + c);
-
+        PatientNameTV = (TextView) findViewById(R.id.add_presc_pname);
+        
+        // Getting current date
         SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault());
         final String formattedDate = df.format(c);
 
-        PatientNameTV = (TextView) findViewById(R.id.add_presc_pname);
+        // Getting patient's fullname from the realtime database and current date for rendering
+        
         patientRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -88,7 +90,8 @@ public class AddPrescActivity extends AppCompatActivity {
 
             }
         });
-
+        
+        // Getting the fullname of the doctor from the DB
         DatabaseReference docRef = FirebaseDatabase.getInstance().getReference().child("Doctors").child(currentUserID);
         docRef.addValueEventListener(new ValueEventListener() {
             @Override
@@ -106,7 +109,8 @@ public class AddPrescActivity extends AppCompatActivity {
             }
         });
         Toast.makeText(this, "Patient ID: " + patient_id, Toast.LENGTH_SHORT).show();
-
+        
+        // Submitting the prescriptions
         SignPrescButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -119,7 +123,8 @@ public class AddPrescActivity extends AppCompatActivity {
                     progressDialog.show();
                     final DatabaseReference prescRef = FirebaseDatabase.getInstance().getReference()
                             .child("Patients").child(patient_id).child("Prescriptions");
-
+                    
+                    // Creating unique QR code for the prescription
                     QRGEncoder qrgEncoder = new QRGEncoder(currentUserID, null, QRGContents.Type.TEXT, 100);
                     bitmap = qrgEncoder.getBitmap();
                     ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -127,13 +132,8 @@ public class AddPrescActivity extends AppCompatActivity {
                     bitmap.compress(Bitmap.CompressFormat.PNG,100,baos);
                     byte[] arr = baos.toByteArray();
                     qrStorage.child("QR").child(currentUserID).putBytes(arr);
-                   /* urll = qrStorage.child("QR").child(currentUserID).getDownloadUrl().getResult().toString();
-                            *//*addOnSuccessListener(new OnSuccessListener<Uri>() {
-                                @Override
-                                public void onSuccess(Uri uri) {
-                                    urll = uri.get;
-                                }
-                            });*/
+                   
+                    // Storing the prescription to the database using a HashMap
                     HashMap hashMap = new HashMap();
                     hashMap.put("prescriptions",prescriptions);
                     hashMap.put("dr_name",dr_name);
@@ -145,12 +145,14 @@ public class AddPrescActivity extends AppCompatActivity {
                                 public void onComplete(@NonNull Task task) {
                                     if (task.isSuccessful())
                                     {
+                                        // If the prescription is stored successfully, redirect to the DashboardActivity
                                         Intent homeIntent = new Intent(AddPrescActivity.this, DashboardActivity.class);
                                         homeIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                                         startActivity(homeIntent);
                                     }
                                     else
                                     {
+                                        // Toasts the error
                                         progressDialog.hide();
                                         String msg = task.getException().getMessage();
                                         Toast.makeText(AddPrescActivity.this, msg, Toast.LENGTH_SHORT).show();
